230fda64ddcee81ccb6037fff38ff4328e6e135e
NellyChen <77566624+Huajuan337@users.noreply.github.com>
2022-09-17 23:51:33 -0700

    Add about_me button

--- app/src/main/java/edu/northeastern/numad22fa_huajuanchen/MainActivity.java
+++ app/src/main/java/edu/northeastern/numad22fa_huajuanchen/MainActivity.java
@@ -3,12 +3,26 @@ package edu.northeastern.numad22fa_huajuanchen;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn_aboutMe;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_aboutMe = (Button) findViewById(R.id.button_aboutMe);
+
+        btn_aboutMe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Name: Huajuan Chen; Email: chen.huaj@gnortheastern.edu", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
